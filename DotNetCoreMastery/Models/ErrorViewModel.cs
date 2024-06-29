1407453c4fcb10ce6a89d8481c3d723c41366f0a
chrisgauthier742018 <chris@elektrus.ca>
2024-06-29 14:32:24 -0500

    Add project files.

---
+++ DotNetCoreMastery/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DotNetCoreMastery.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
